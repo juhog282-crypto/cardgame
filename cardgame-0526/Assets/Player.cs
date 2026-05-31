1b1912859af53b29a4c6ba4a5bb33cf75510e92f
juhog282 <juhog282@g.shingu.ac.kr>
2026-05-31 18:50:26 +0900

    0531

---
+++ cardgame-0526/Assets/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3f;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back* speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+}
